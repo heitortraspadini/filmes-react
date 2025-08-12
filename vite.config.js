b1539526ecead57727dcf78abbe845b46d54f1c3
heitortraspadini <heitorg.traspadini@gmail.com>
2025-08-12 11:16:15 -0300

    commit inicial

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/meu-primeiro-react/',
+  base: '/filmes-react/',
 })
